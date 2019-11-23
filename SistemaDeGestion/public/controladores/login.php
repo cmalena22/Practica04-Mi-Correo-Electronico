cd429f7aad9ebfda53fbe13f1d5f7dea8c9e3584
Cinthia Iza <cizac1@est.ups.edu.ec>
2019-11-23 08:32:39 -0500

    PHP  para login-

--- SistemaDeGestion/public/controladores/login.php
+++ SistemaDeGestion/public/controladores/login.php
@@ -1,10 +1,14 @@
+<?php     session_start();     
+if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){                 
+    header("Location: /SistemaDeGestion/public/vista/login.html");             
+    } 
+?>
 <?php     session_start(); 
  
     include '../../config/conexionBD.php'; 
  
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;     
-    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null; 
- 
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;  
     $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password = '$contrasena'"; 
  
     $result = $conn->query($sql);            
